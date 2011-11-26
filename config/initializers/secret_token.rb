2bb7a937deb0c11d45a0f246e81079118bbf20a5
Cheng Chen <mr.chengchen@gmail.com>
2011-11-26 16:36:14 -0500

    First commit, adding to GitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '49475f2eacee318ec5e67fc3d2e7fa47f1bf5252a2cfb217bf16e68f41cbe03f755483417e9ef349789d796e4ae2351a3598cc477fdda6502a04eabb00bb2612'
